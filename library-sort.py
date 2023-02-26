3eefe95759a59965115bfc1fd24aed819007acb9
Kroeger-IT <126395279+Kroeger-IT@users.noreply.github.com>
2023-02-26 16:00:43 +0100

    Library sort example Python 3
    Note that the Library Sort algorithm has a time complexity of O(n^2) in the worst case, which makes it less efficient than other sorting algorithms like QuickSort or MergeSort. However, it can be useful in some specialized cases where the size of the input data is relatively small or the range of the input data is very limited.

---
+++ library-sort.py
@@ -0,0 +1,14 @@
+def library_sort(arr):
+    sorted_arr = []
+    while arr:
+        smallest = min(arr)
+        sorted_arr.append(smallest)
+        arr.remove(smallest)
+    return sorted_arr
+  
+  
+  # Example Usage
+  
+  arr = [4, 2, 7, 1, 3, 5, 6]
+sorted_arr = library_sort(arr)
+print(sorted_arr)
